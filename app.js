a00d6719e6553579203a72823023f48df9dec8c6
Benjamin SA <benjamynfrank101@gmail.com>
2024-02-16 10:43:32 +0100

    Fix network error

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const themeButton = document.querySelector("#theme-btn");
 const deleteButton = document.querySelector("#delete-btn");
 
 let userText = null;
-const API_KEY = "PASTE-YOUR-API-KEY-HERE"; // Paste your API key here
+const API_KEY = "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"; // Paste your API key here
 
 const loadDataFromLocalstorage = () => {
     // Load saved chats and theme from local storage and apply/add on the page
